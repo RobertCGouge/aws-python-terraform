c9002485d8e8cd4c686c72b2b70c9667e754a932
Robert C. Gouge <robert.c.gouge@gmail.com>
2019-07-26 11:52:10 -0500

    run environment variable logic before creating S3 bucket name

--- upload_template.py
+++ upload_template.py
@@ -1,7 +1,12 @@
 import boto3
 import os
+import datetime
+import config_environment
 
 s3 = boto3.client('s3')
 
+config_environment.config_environment()
+
 filename = 'terraform_host.yml'
-bucket_name = #TODO add environment variable logic to create s3 bucket name
+envrionment = os.environ.get('Environment')
+bucket_name = str(os.environ.get('Environment')) + str(os.environ.get('Client')) + str(os.environ.get('Storage')) + str(datetime.datetime.now().strftime("%Y-%m-%d"))
