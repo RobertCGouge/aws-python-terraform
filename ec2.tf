43e0244035170fc276dc601dbe20bef119f2dc37
Robert Gouge <robert.c.gouge@gmail.com>
2019-07-27 12:44:29 -0500

    update filter

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@ data "aws_ami" "al2" {
 
     filter {
         name   = "name"
-        values = ["/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"]
+        values = ["amzn2-ami-hvm-x86_64-gp2"]
     }
 
     filter {
