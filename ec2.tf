93714cef49dfe077e485be4d99aca8fe920709a6
Robert Gouge <robert.c.gouge@gmail.com>
2019-07-27 12:37:53 -0500

    Try new things

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,25 @@ provider "aws" {
     profile = "default"
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0b898040803850657"
+data "aws_ami" "al2" {
+    most_recent = true
+
+    filter {
+        name   = "name"
+        values = ["/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+
+}
+resource "aws_instance" "web" {
+    ami           = "${data.aws_ami.al2.id}"
     instance_type = "t2.micro"
+
+    tags = {
+        Name = "HelloWorld"
+    }
 }
